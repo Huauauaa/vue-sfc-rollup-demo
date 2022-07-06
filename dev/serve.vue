f25b173f6e7460a5c64f76fcdaf14a2b6fe968b8
Huaweiwei <harvey0379@163.com>
2022-07-06 23:00:55 +0800

    init

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { VueSfcRollupDemoSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  VueSfcRollupDemoSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-sfc-rollup-demo-sample />
+  </div>
+</template>
