dfc4f8755dde9caec7cad772abf2ebf8c28e083c
huaweiwei <harvey0379@163.com>
2022-07-10 22:34:13 +0800

    init

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { VueSfcRollupDemoSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  VueSfcRollupDemoSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-sfc-rollup-demo-sample />
+  </div>
+</template>
